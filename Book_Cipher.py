bd0eccb88203552dca4a3b9021f4602113caaf28
William Martens <68499986+loneicewolf@users.noreply.github.com>
2020-11-06 10:58:28 +0100

    Update Book_Cipher.py

--- Book_Cipher.py
+++ Book_Cipher.py
@@ -2,9 +2,13 @@
 # this is kinda like "Only Code" for those interested
 # Shortened it and removed stuff to make it more 'elegant' (hopefully it became that too)
 
+# Recommending this link. Provided a full very good and detailed question. https://stackoverflow.com/q/42926663/14346786
+# It is the question I have origininally been following for more than 2 years+.  Wished someone would just 'come along with a solution'.. But that never happend.
+# In this case, It was myself who came up with a (hopefully, acceptably- Okay) Solution. Or answer. 
+# If you like this, or if this is/will/was helpful - In any **possible way** PLEASE tell me that! Either via mail, or just about anything.
+# Thank you!
 
-# Replace "document1.txt" with whatever your book / document's name is.
-BOOK="document1.txt" # This contains your "Word Word Word Word ...." I believed from the very start that you meant, they are not the same - (obviously)
+BOOK="document1.txt"
 
 # Read book into "boktxt"
 def GetBookContent(BOOK):
