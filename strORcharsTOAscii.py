48fbdab5ebf1545ae2583698039a0d4b7cb84095
William Martens <68499986+loneicewolf@users.noreply.github.com>
2021-01-11 08:48:56 +0100

    Create strORcharsTOAscii.py

---
+++ strORcharsTOAscii.py
@@ -0,0 +1,43 @@
+def Pri(s1):
+	for c in s1:
+		print( ord(c) )
+
+# usage
+# Pri('a')
+# or
+# Pri("Hello World")
+# or
+# s1= "a string here!"
+# for c in s1:
+#   Pri(c)
+
+# test run #
+
+# >>> s1="A phrase here thanks"
+# >>> for c in s1:
+# ...     Pri(c)
+# ... 
+# 65
+# 32
+# 112
+# 104
+# 114
+# 97
+# 115
+# 101
+# 32
+# 104
+# 101
+# 114
+# 101
+# 32
+# 116
+# 104
+# 97
+# 110
+# 107
+# 115
+# >>> 
+# 
+
+
